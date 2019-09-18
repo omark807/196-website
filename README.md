6e45f28e6a598fe14e9d60e90db2fc2a69026d81
OmarK807 <omark807@gmail.com>
2019-09-18 10:55:17 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,3 @@
+# website
+
+Repo containing the course website.
---
+++ server.py
@@ -0,0 +1,11 @@
+import os
+import git
+import logging
+if __name__ == "__main__":
+    g = git.cmd.Git(".")
+    while True:
+        logging.error("\033[31mRELOADING SERVER\033[0m")
+        status = os.system("python3 app.py")
+        if status: break
+        g.pull()
+        os.system("git pull https://gitlab.engr.illinois.edu/cs196/website.git") 
